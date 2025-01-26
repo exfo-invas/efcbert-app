11b80483637040c9f46cc8bcc25849666f1ed55d
iamdevkach <achyuth@softqlud.com>
2025-01-26 17:44:03 -0600

    new modules imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,65 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {EventComponent} from "./event/event.component";
+import {providePrimeNG} from "primeng/config";
+import Aura from "@primeng/themes/aura";
+import {InputText} from "primeng/inputtext";
+import {Card} from "primeng/card";
+import {TableModule} from "primeng/table";
+import {Button} from "primeng/button";
+import {Message} from "primeng/message";
+import {Panel} from "primeng/panel";
+import {Tooltip} from "primeng/tooltip";
+import {Toast} from "primeng/toast";
+import {ConfirmPopup} from "primeng/confirmpopup";
+import {NgOptimizedImage} from "@angular/common";
+import {LoginPageComponent} from "./shared/login-page/login-page.component";
 
-@NgModule({ declarations: [
-        AppComponent,
-        DashboardComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        FormsModule], providers: [
-        provideHttpClient(withInterceptorsFromDi())
-    ] })
-export class AppModule { }
-
-/*providePrimeNG({ theme: {
-    preset: Aura
-  }}),
-  provideAnimationsAsync()*/
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    EventComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    InputText,
+    Card,
+    TableModule,
+    Button,
+    Message,
+    Panel,
+    Tooltip,
+    Toast,
+    ConfirmPopup,
+    NgOptimizedImage,
+    LoginPageComponent,
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
+  ]
+})
+export class AppModule {
+}
