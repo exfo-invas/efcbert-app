f3c4008bba09a955d06a030ff88890f481a8e9ba
iamdevkach <achyuth@softqlud.com>
2025-01-26 17:42:49 -0600

    Dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../service/api.service";
+import {Router} from "@angular/router";
+import {ConnectionTCP} from "../service/api.service.model";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: false,
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   title = 'efcbert-app';
   ipAddress: string = '';
   portNum: string = '';
-  reponseData: string = '';
+  reponseData: ConnectionTCP = new ConnectionTCP();
+  connStatus: boolean;
+  isNew: boolean = false;
+  command: string;
+  commandResponse: string = "";
 
   constructor(private apiService: ApiService) {
   }
 
   ngOnInit() {
+    if (this.ipAddress === '' && this.portNum === '') {
+      this.isNew = false;
+    }
   }
 
-  getConnection(ipAddress: string, portNum: string) {
-
-    this.apiService.get(ipAddress, portNum).subscribe((data: any) => {
+  getConnection() {
+    this.apiService.get(this.ipAddress, this.portNum).subscribe((data: ConnectionTCP) => {
       console.log(data);
-      this.reponseData = data.status;
+      this.isNew = true;
+      this.connStatus = data.status;
+      this.reponseData = data;
     });
-
   }
 
+  executeCommand() {
+    this.apiService.executeCommand(this.command).subscribe((data: string) => {
+      console.log(data);
+      this.commandResponse = data;
+    });
+  }
+
+  closeConnect() {
+    this.apiService.close().subscribe((data: string) => {
+      console.log(data);
+      if (data === 'true') {
+        this.default();
+      } else {
+        console.log('error closing connection');
+      }
+      return data;
+    });
+  }
+
+  default() {
+    this.ipAddress = '';
+    this.portNum = '';
+    this.isNew = false;
+    this.connStatus = false;
+    this.command = '';
+    this.commandResponse = '';
+  }
 }
