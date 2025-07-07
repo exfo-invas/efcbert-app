0096b5717fc80f910b30fbea431f8af290e8e8a9
Achyuth <imdevkach@gmail.com>
2025-07-06 23:11:31 -0500

    new table format, api calls

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../service/api.service";
 
-interface DataRow {
+interface ServiceDisruption {
   type: string;
   speed: string;
   frameSize: string;
@@ -9,67 +10,210 @@ interface DataRow {
   throughput: string;
 }
 
+interface TrafficDisruption {
+  type: string;
+  fcRate: string;
+  protocol: string;
+  encoding: string;
+  actualThroughput: string;
+  actualTransferSpeed: string;
+  lineSpeed: string;
+  currentUtilization: string;
+  measuredThroughput: string;
+  transferSpeed: string;
+  measuredLineSpeed: string;
+}
+
+interface ThroughPut {
+  type: string;
+  fcRate: string;
+  frameSize: string;
+  fullLineRate: string;
+  measureRate: string;
+  framesLossRate: string;
+}
+
+interface FrameLoss {
+  type: string;
+  frameRate: string;
+  totalTransmittedBytes: string;
+  framesTransmitted: string;
+}
+
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss',
   standalone: false
 })
-export class EventComponent {
+export class EventComponent implements OnInit {
 
-  columns = [
-    { field: 'fcRate' , header: 'FC Rate' , suffix: 'Gbps' },
-    { field: 'frameSize', header: 'Frame Size' , suffix: 'Bytes' },
-    { field: 'throughputRate' , header: 'Throughput rate' , suffix: 'Gbps' },
-    { field: 'lineRate' , header: 'Measure line rate' , suffix: 'Gbps' },
-    { field: 'measuredRate' , header: 'Measure rate' , suffix: 'Gbps' }
-  ];
-
-  throughPuts: any[] = [{
-    fcRate: 0,
-    frameSize: 0,
-    throughputRate: 10,
-    lineRate: 0,
-    measuredRate: 0
-  }];
-
-  columnsFramesLoss = [
-    { field: 'fcRate', header: 'FC Rate', suffix: 'Gbps' },
-    { field: 'frameSize', header: 'Frame Size' , suffix: 'Bytes' },
-    { field: 'fullLineRate', header: 'Full Line rate' , suffix: 'Gbps' },
-    { field: 'framesSecs', header: 'Frames/sec' , suffix: 'Frames/sec' },
-    { field: 'framesLost', header: 'Frames Lost' , suffix: 'Frames' },
-    { field: 'frameLossRate', header: 'Frame loss percent' , suffix: '%' }
-  ];
-
-  frameLosses: any[] = [{
-    fcRate: 0,
-    frameSize: 0,
-    fullLineRate: 10,
-    framesSecs: 0,
-    framesLost: 0,
-    frameLossRate: 0
-  }];
-
-  tableData: DataRow[] = [
+  throughPuts: ThroughPut[] = [
     {
       type: 'Tx',
-      speed: '1X (850Mbps)',
-      frameSize: 'LINS1:SOUR:DATA:TEL:FIB:STR:SIZE?',
-      lineDataRate: 'Cal Table',
-      txUtilization: 'Command TX',
-      throughput: 'Cal Table',
+      fcRate: '-',
+      frameSize: '-',
+      fullLineRate: '-',
+      measureRate: '-',
+      framesLossRate: '-'
     },
     {
       type: 'Rx',
-      speed: '1X (850Mbps)',
-      frameSize: 'LINS1:SOUR:DATA:TEL:FIB:STR:SIZE?',
-      lineDataRate: 'Cal Table',
-      txUtilization: 'Command RX',
-      throughput: 'Cal Table',
+      fcRate: '-',
+      frameSize: '-',
+      fullLineRate: '-',
+      measureRate: '-',
+      framesLossRate: '-'
+    }];
+
+  frameLosses: FrameLoss[] = [
+    {
+      type: 'Tx',
+      frameRate: '-',
+      totalTransmittedBytes: '-',
+      framesTransmitted: '-'
+    },
+    {
+      type: 'Rx',
+      frameRate: '-',
+      totalTransmittedBytes: '-',
+      framesTransmitted: '-'
+    }];
+
+  serviceDisruptions: ServiceDisruption[] = [
+    {
+      type: 'Tx',
+      speed: '-',
+      frameSize: '-',
+      lineDataRate: '-',
+      txUtilization: '-',
+      throughput: '-'
+    },
+    {
+      type: 'Rx',
+      speed: '-',
+      frameSize: '-',
+      lineDataRate: '-',
+      txUtilization: '-',
+      throughput: '-',
     },
   ];
 
-  constructor() { }
+  trafficDisruptions: TrafficDisruption[] = [
+    {
+      type: 'Tx',
+      fcRate: '-',
+      protocol: '-',
+      encoding: '-',
+      actualThroughput: '-',
+      actualTransferSpeed: '-',
+      lineSpeed: '-',
+      currentUtilization: '-',
+      measuredThroughput: '-',
+      transferSpeed: '-',
+      measuredLineSpeed: '-'
+    },
+    {
+      type: 'Rx',
+      fcRate: '-',
+      protocol: '-',
+      encoding: '-',
+      actualThroughput: '-',
+      actualTransferSpeed: '-',
+      lineSpeed: '-',
+      currentUtilization: '-',
+      measuredThroughput: '-',
+      transferSpeed: '-',
+      measuredLineSpeed: '-'
+    }
+  ];
 
+  constructor(private apiService: ApiService) {
+  }
+
+  //Every Second, fetch the latest data for throughput, frame loss, service disruption, and traffic disruption
+  ngOnInit(): void {
+    setInterval(() => {
+      this.getThroughput();
+      this.getFrameLoss();
+      this.getServiceDisruptionCommand();
+      this.getTrafficDisruption();
+    }, 1000);
+  }
+
+  getServiceDisruptionCommand(): void {
+    this.apiService.getServiceDisruption().subscribe({
+      next: (response: ServiceDisruption[]) => {
+        console.log(`Service disruption command sent successfully for :`, response);
+        if (response && response.length > 0) {
+          response.forEach((service) => {
+            const index = service.type === 'Tx' ? 0 : 1;
+            if (this.serviceDisruptions[index]) {
+              Object.assign(this.serviceDisruptions[index], service);
+            }
+          });
+        }
+      },
+      error: (error) => {
+        console.error(`Error sending service disruption command for:`, error);
+      }
+    });
+  }
+
+  getTrafficDisruption(): void {
+    this.apiService.getTrafficDisruption().subscribe({
+      next: (response: TrafficDisruption[]) => {
+        console.log(`Traffic disruption data for :`, response);
+        if (response && response.length > 0) {
+          response.forEach((disruption) => {
+            const index = disruption.type === 'Tx' ? 0 : 1;
+            if (this.trafficDisruptions[index]) {
+              Object.assign(this.trafficDisruptions[index], disruption);
+            }
+          });
+        }
+      },
+      error: (error) => {
+        console.error(`Error fetching traffic disruption data for:`, error);
+      }
+    });
+  }
+
+  getThroughput(): void {
+    this.apiService.getThroughput().subscribe({
+      next: (response:ThroughPut[] ) => {
+        console.log(`Throughput data for :`, response);
+        if (response && response.length > 0) {
+          response.forEach((throughput) => {
+            const index = throughput.type === 'Tx' ? 0 : 1;
+            if (this.throughPuts[index]) {
+              Object.assign(this.throughPuts[index], throughput);
+            }
+          });
+        }
+      },
+      error: (error) => {
+        console.error(`Error fetching throughput data for:`, error);
+      }
+    });
+  }
+
+  getFrameLoss(): void {
+    this.apiService.getFrameLoss().subscribe({
+      next: (response: FrameLoss[]) => {
+        console.log(`Frame loss data for :`, response);
+        if (response && response.length > 0) {
+          response.forEach((frameLoss) => {
+            const index = frameLoss.type === 'Tx' ? 0 : 1;
+            if (this.frameLosses[index]) {
+              Object.assign(this.frameLosses[index], frameLoss);
+            }
+          });
+        }
+      },
+      error: (error) => {
+        console.error(`Error fetching frame loss data for:`, error);
+      }
+    });
+  }
 }
