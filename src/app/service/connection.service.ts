6ec83b672ede6d6c9ee5e196b45021dd69e1e72e
achyuth yallambhotla <achyuth@softqlud.com>
2025-12-13 15:21:12 -0600

    app health app

--- src/app/service/connection.service.ts
+++ src/app/service/connection.service.ts
@@ -11,6 +11,8 @@ export class ConnectionService {
   ipaddress: string;
   port: string;
   status: boolean;
+  serverHealth: boolean;
+  count: number = 0;
 
   setConnection(ipaddress: string, status: boolean) {
     this.ipaddress = ipaddress;
@@ -31,21 +33,55 @@ export class ConnectionService {
     this.port = '';
   }
 
+  setHealth(health: boolean) {
+    this.serverHealth = health;
+  }
+
+  getHealth() {
+    this.getAppHealth();
+    return this.serverHealth;
+  }
+
+  incrementCount() {
+    this.count++;
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  resetCount() {
+    this.count = 0;
+  }
+
   private getLatestStatus() {
     this.apiService.status().subscribe((data: string) => {
-        console.log(data);
+        console.log("connection.service:getAppHealth: Connection service getLatestStatus() to fetch status", data);
         if (data === 'true') {
           this.status = true;
 
         } else {
           this.status = false;
-          console.log('error closing connection');
+          console.log('connection.service:getAppHealth: error closing connection');
         }
       },
       (error) => {
         this.status = false;
-        console.log('error closing connection');
+        console.log('connection.service:getAppHealth: error closing connection');
         console.log('Error message:', error.message, 'Status:', error.status);
       });
+  }
+
+  getAppHealth() {
+    this.apiService.healthCheck().subscribe({
+      next: (data: any) => {
+        console.log('connection.service:getAppHealth: Health check data:', data);
+        this.setHealth(data.status === 'UP');
+      },
+      error: (error) => {
+        console.error('connection.service:getAppHealth: Error during health check:', error);
+        this.setHealth(false);
+      }
+    });
   }
 }
